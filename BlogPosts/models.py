90cb56fbfdf36db81ee6d8faf295790a8f73824d
Galladaeus <galladaeus@hotmail.com>
2018-02-12 06:14:26 -0500

    HTML Test mainpage added

--- BlogPosts/models.py
+++ BlogPosts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+import datetime
+
+
+# Full blog post information
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    pub_date = models.DateTimeField(auto_now=True)
+    content = models.TextField
+
+    # Check whether post is older than a month
+    @staticmethod
+    def old_post(self):
+        now = timezone.now()
+        return now - datetime.timedelta(months=1) <= self.pub_date <= now
